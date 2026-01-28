275fea37ddbcbb3921b3c323b158b5b6a7edcf26
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-01-28 14:15:12 +0530

    inititated prisma

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,5 +1,9 @@
+
+function setAppConfig(){}
+
 export const appConfig = {
     port: process.env.PORT || 5001,
-    nodeEnv: process.env.NODE_ENV || "development",
+    nodeEnv: process.env.NODE_ENV || "dev",
     microserviceName: process.env.MICROSERVICE_NAME ? process.env.MICROSERVICE_NAME + "microservice" : "server"
 }
+
